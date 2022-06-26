0af3d3ae122d6418f5c2591aad6784d616a7905f
Garrett Newman <garrett.s.newman@gmail.com>
2022-06-26 15:29:44 -0600

    Fix repeating keys

--- ui/src/features/builder/useTileInput.ts
+++ ui/src/features/builder/useTileInput.ts
@@ -20,26 +20,42 @@ export default function useTileInput(
   setNextPrimarySelectedTile: SetNextPrimarySelectedTileType
 ) {
   const dispatch = useDispatch();
+  // A map of key states, used to prevent rapid-fire keypresses by olding keys
+  // down
+  const keyStates = useRef<{ [key: string]: 'up' | 'down' }>({});
+  // We use a queue so that we don't miss any keystrokes--otherwise, we would
+  // miss keystrokes that happened before this hook had a chance to rerun
   const cachedInputQueue = useRef<string[]>([]);
   const [inputQueue, setInputQueue] = useState<string[]>([]);
 
   const onKeyDown = useCallback(
     (event) => {
-      if (!_.includes([...ALL_LETTERS, '.', 'Backspace'], event.key)) return;
-      cachedInputQueue.current.push(event.key);
+      // TODO: Replace tab usage here with moving between clues with tab
+      if (!_.includes([...ALL_LETTERS, '.', 'Backspace', 'Tab'], event.key))
+        return;
+      event.preventDefault();
+      if (keyStates.current[event.key] === 'down') return;
+      keyStates.current[event.key] = 'down';
+
+      // Add valid inputs to the queue.
+      const key =
+        event.key === 'Tab' && event.shiftKey ? 'ShiftTab' : event.key;
+      cachedInputQueue.current.push(key);
       setInputQueue(cachedInputQueue.current);
     },
     [setInputQueue]
   );
+  const onKeyUp = useCallback((event) => {
+    keyStates.current[event.key] = 'up';
+  }, []);
 
+  // Churn through queue
   useEffect(() => {
     if (!selectedTilesState || cachedInputQueue.current.length === 0) {
-      console.log('skip', cachedInputQueue.current);
       cachedInputQueue.current = [];
       if (inputQueue.length > 0) setInputQueue([]);
       return;
     }
-    console.log('process', cachedInputQueue.current);
 
     let primaryIndex = selectedTilesState.primaryIndex;
     const tileUpdates = _.compact(
@@ -57,6 +73,11 @@ export default function useTileInput(
           };
           if (primaryIndex > 0 && erasePrevious) primaryIndex -= 1;
           return tileUpdate;
+        } else if (key === 'Tab') {
+          if (primaryIndex < selectedTilesState.locations.length - 1)
+            primaryIndex += 1;
+        } else if (key === 'ShiftTab') {
+          if (primaryIndex > 0) primaryIndex -= 1;
         } else {
           const tileUpdate: TileUpdateType = {
             ...selectedTilesState.locations[primaryIndex],
@@ -69,7 +90,7 @@ export default function useTileInput(
       })
     );
     cachedInputQueue.current = [];
-    setInputQueue([]);
+    setInputQueue(cachedInputQueue.current);
 
     dispatch(setPuzzleTileValues(tileUpdates));
     setNextPrimarySelectedTile(primaryIndex - selectedTilesState.primaryIndex);
@@ -84,8 +105,10 @@ export default function useTileInput(
 
   useEffect(() => {
     document.addEventListener('keydown', onKeyDown);
+    document.addEventListener('keyup', onKeyUp);
     return () => {
       document.removeEventListener('keydown', onKeyDown);
+      document.removeEventListener('keyup', onKeyUp);
     };
-  }, [onKeyDown]);
+  }, [onKeyDown, onKeyUp]);
 }
