e9c507185c45e8793f339f917fb7d0a612208cb7
Garrett Newman <garrett.s.newman@gmail.com>
2022-08-10 18:44:58 -0600

    Install firebase

---
+++ ui/src/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCzJ-k6IvZRzdT-69Y9TBB0t9ttFg4mVHw',
+  authDomain: 'crosswyrd-9a447.firebaseapp.com',
+  projectId: 'crosswyrd-9a447',
+  storageBucket: 'crosswyrd-9a447.appspot.com',
+  messagingSenderId: '816152191251',
+  appId: '1:816152191251:web:af948e154a372d58803ae6',
+  measurementId: 'G-QX9YGB4C8X',
+};
+
+export default function initFirebase() {
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  getAnalytics(app);
+}
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -5,8 +5,10 @@ import { Provider } from 'react-redux';
 
 import App from './features/app/App';
 import { store } from './app/store';
+import initFirebase from './firebase';
 import './index.css';
 
+initFirebase();
 ReactDOM.render(
   <Provider store={store}>
     <HotkeysProvider dialogProps={{ isOpen: true }}>
